ceac5f1e91de9d4add19aa7f6014e909c03561ec
Denis Kuzin <gyroin@gmail.com>
2023-06-02 23:05:30 +0800

    на сегодня все

--- bot/play_field.py
+++ bot/play_field.py
@@ -5,11 +5,20 @@ from typing import List
 class PlayField:
     
     def __init__(self):
-        self._buttons = [[InlineKeyboardButton(text='.', callback_data='11'), InlineKeyboardButton(text='.', callback_data='12'), InlineKeyboardButton(text='.', callback_data='13')],
-                   [InlineKeyboardButton(text='.', callback_data='21'), InlineKeyboardButton(text='.', callback_data='22'), InlineKeyboardButton(text='.', callback_data='23')],
-                   [InlineKeyboardButton(text='.', callback_data='31'), InlineKeyboardButton(text='.', callback_data='32'), InlineKeyboardButton(text='.', callback_data='33')]]
-        self._keyboard_markup = InlineKeyboardMarkup(inline_keyboard=self._buttons)
+        self._battle_field = [['.', '.', '.'],
+                              ['.', '.', '.'],
+                              ['.', '.', '.']]
+        self._keyboard_markup = InlineKeyboardMarkup(inline_keyboard=self._generate_buttons())
         self._states_changed: List[str] = []
+    
+    def _generate_buttons(self):
+        buttons = []
+        for x in range(3):
+            line = []
+            for y in range(3):
+                line.append(InlineKeyboardButton(text=self._battle_field[x][y], callback_data=f'{x + 1}{y + 1}'))
+            buttons.append(line)
+        return buttons    
     
     def get_battle_field(self) -> InlineKeyboardMarkup:
         return self._keyboard_markup
@@ -23,9 +32,20 @@ class PlayField:
     def _change_state(self, x: int, y: int, symbol: str):
         sbtn = f'{x}{y}'
         if not sbtn in self._states_changed: 
-            self._buttons[x - 1][y - 1] = InlineKeyboardButton(text=symbol, callback_data=sbtn)
-            self._keyboard_markup = InlineKeyboardMarkup(inline_keyboard=self._buttons)
+            self._battle_field[x - 1][y - 1] = symbol
+            self._keyboard_markup = InlineKeyboardMarkup(inline_keyboard=self._generate_buttons())
             self._states_changed.append(sbtn)
             return self._keyboard_markup
         else:
             return self._keyboard_markup
+
+    def check_win(self) -> int:
+        bf = self._battle_field
+        if ((bf[0][0] == 'X' and bf[0][1] == 'X' and bf[0][2] == 'X') 
+            or (bf[1][0] == 'X' and bf[1][1] == 'X' and bf[1][2] == 'X')
+            or (bf[2][0] == 'X' and bf[2][1] == 'X' and bf[2][2] == 'X')
+            or (bf[0][0] == 'X' and bf[1][0] == 'X' and bf[2][0] == 'X')
+            or (bf[0][1] == 'X' and bf[1][1] == 'X' and bf[2][1] == 'X')
+            or (bf[0][2] == 'X' and bf[1][2] == 'X' and bf[2][2] == 'X')):
+                return 1
+        pass
--- bot/xobot.py
+++ bot/xobot.py
@@ -7,10 +7,12 @@ bot = Bot(token=TGID)
 dp = Dispatcher(bot)
 
 users: List[Tuple[int, Coroutine]] = []
+move = -1
 pf = PlayField()
 
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
+    global move
     if len(users) < 1:
         msg = await bot.send_message(message.from_user.id, 'Игра крестики-нолики, ждем второго игрока')
         users.append((message.from_user.id, msg))
@@ -19,88 +21,53 @@ async def start(message: types.Message):
         await users[0][1].edit_reply_markup(pf.get_battle_field())
         msg = await bot.send_message(message.from_user.id, 'Игра крестики-нолики, играем О, ходим вторыми', reply_markup=pf.get_battle_field())
         users.append((message.from_user.id, msg))
+        move = 0
 
 
 @dp.callback_query_handler(text='11')
 async def btn11_clicked(query: types.CallbackQuery):
-    if query.from_user.id == users[0][0]:
-        await users[0][1].edit_reply_markup(pf.change_x_state(1, 1))
-        await users[1][1].edit_reply_markup(pf.change_x_state(1, 1))
-    else:
-        await users[0][1].edit_reply_markup(pf.change_o_state(1, 1))
-        await users[1][1].edit_reply_markup(pf.change_o_state(1, 1))
+    await move_done(query, 1, 1)
 
 @dp.callback_query_handler(text='12')
-async def btn11_clicked(query: types.CallbackQuery):
-    if query.from_user.id == users[0][0]:
-        await users[0][1].edit_reply_markup(pf.change_x_state(1, 2))
-        await users[1][1].edit_reply_markup(pf.change_x_state(1, 2))
-    else:
-        await users[0][1].edit_reply_markup(pf.change_o_state(1, 2))
-        await users[1][1].edit_reply_markup(pf.change_o_state(1, 2))
+async def btn12_clicked(query: types.CallbackQuery):
+    await move_done(query, 1, 2)
 
 
 @dp.callback_query_handler(text='13')
-async def btn11_clicked(query: types.CallbackQuery):
-    if query.from_user.id == users[0][0]:
-        await users[0][1].edit_reply_markup(pf.change_x_state(1, 3))
-        await users[1][1].edit_reply_markup(pf.change_x_state(1, 3))
-    else:
-        await users[0][1].edit_reply_markup(pf.change_o_state(1, 3))
-        await users[1][1].edit_reply_markup(pf.change_o_state(1, 3))
+async def btn13_clicked(query: types.CallbackQuery):
+    await move_done(query, 1, 3)
     
 @dp.callback_query_handler(text='21')
-async def btn11_clicked(query: types.CallbackQuery):
-    if query.from_user.id == users[0][0]:
-        await users[0][1].edit_reply_markup(pf.change_x_state(2, 1))
-        await users[1][1].edit_reply_markup(pf.change_x_state(2, 1))
-    else:
-        await users[0][1].edit_reply_markup(pf.change_o_state(2, 1))
-        await users[1][1].edit_reply_markup(pf.change_o_state(2, 1))
+async def btn21_clicked(query: types.CallbackQuery):
+    await move_done(query, 2, 1)
 
 @dp.callback_query_handler(text='22')
-async def btn11_clicked(query: types.CallbackQuery):
-    if query.from_user.id == users[0][0]:
-        await users[0][1].edit_reply_markup(pf.change_x_state(2, 2))
-        await users[1][1].edit_reply_markup(pf.change_x_state(2, 2))
-    else:
-        await users[0][1].edit_reply_markup(pf.change_o_state(2, 2))
-        await users[1][1].edit_reply_markup(pf.change_o_state(2, 2))
-
+async def btn22_clicked(query: types.CallbackQuery):
+    await move_done(query, 2, 2)
 
 @dp.callback_query_handler(text='23')
-async def btn11_clicked(query: types.CallbackQuery):
-    if query.from_user.id == users[0][0]:
-        await users[0][1].edit_reply_markup(pf.change_x_state(2, 3))
-        await users[1][1].edit_reply_markup(pf.change_x_state(2, 3))
-    else:
-        await users[0][1].edit_reply_markup(pf.change_o_state(2, 3))
-        await users[1][1].edit_reply_markup(pf.change_o_state(2, 3))
+async def btn23_clicked(query: types.CallbackQuery):
+    await move_done(query, 2, 3)
 
 @dp.callback_query_handler(text='31')
-async def btn11_clicked(query: types.CallbackQuery):
-    if query.from_user.id == users[0][0]:
-        await users[0][1].edit_reply_markup(pf.change_x_state(3, 1))
-        await users[1][1].edit_reply_markup(pf.change_x_state(3, 1))
-    else:
-        await users[0][1].edit_reply_markup(pf.change_o_state(3, 1))
-        await users[1][1].edit_reply_markup(pf.change_o_state(3, 1))
+async def btn31_clicked(query: types.CallbackQuery):
+    await move_done(query, 3, 1)
 
 @dp.callback_query_handler(text='32')
-async def btn11_clicked(query: types.CallbackQuery):
-    if query.from_user.id == users[0][0]:
-        await users[0][1].edit_reply_markup(pf.change_x_state(3, 2))
-        await users[1][1].edit_reply_markup(pf.change_x_state(3, 2))
-    else:
-        await users[0][1].edit_reply_markup(pf.change_o_state(3, 2))
-        await users[1][1].edit_reply_markup(pf.change_o_state(3, 2))
-
+async def btn32_clicked(query: types.CallbackQuery):
+    await move_done(query, 3, 2)
 
 @dp.callback_query_handler(text='33')
-async def btn11_clicked(query: types.CallbackQuery):
-    if query.from_user.id == users[0][0]:
-        await users[0][1].edit_reply_markup(pf.change_x_state(3, 3))
-        await users[1][1].edit_reply_markup(pf.change_x_state(3, 3))
-    else:
-        await users[0][1].edit_reply_markup(pf.change_o_state(3, 3))
-        await users[1][1].edit_reply_markup(pf.change_o_state(3, 3))
+async def btn33_clicked(query: types.CallbackQuery):
+    await move_done(query, 3, 3)
+
+async def move_done(query: types.CallbackQuery, x: int, y: int):
+    global move
+    if (query.from_user.id == users[0][0]) and move == 0:
+        await users[0][1].edit_reply_markup(pf.change_x_state(x, y))
+        await users[1][1].edit_reply_markup(pf.change_x_state(x, y))
+        move = 1
+    if (query.from_user.id == users[1][0]) and move == 1:
+        await users[0][1].edit_reply_markup(pf.change_o_state(x, y))
+        await users[1][1].edit_reply_markup(pf.change_o_state(x, y))
+        move = 0
